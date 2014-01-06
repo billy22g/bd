1c54ebf263168c7ffea79a241bc423bc172becf4
Billy <navyosu@gmail.com>
2014-01-06 13:05:26 -0700

    Added validation for presence of a number in phone number

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,7 @@
 class PhoneNumber < ActiveRecord::Base
+  #relationships
+  
+
+  #validations
+  validates :number, presence: true
 end
--- test/models/phone_number_test.rb
+++ test/models/phone_number_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class PhoneNumberTest < ActiveSupport::TestCase
   
   test "it is invalid without a number" do
-    phone_numbers(:one).update(:name => nil)
+    phone_numbers(:one).update(:number => nil)
     refute phone_numbers(:one).valid?
   end
 
