09239a89f4877c77f1e260c02f593426cd49ed03
Billy <navyosu@gmail.com>
2014-01-06 19:46:31 -0700

    Added validations and relationship for phone numbers to establishments

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,7 +1,8 @@
 class PhoneNumber < ActiveRecord::Base
   #relationships
-  
+  belongs_to :establishment
 
   #validations
   validates :number, presence: true
+  validates :establishment_id, presence: true
 end
