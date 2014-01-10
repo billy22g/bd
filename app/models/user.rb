f993cd956d8f4848e6acdfbc6c5fcaaf02ff67e3
Billy <navyosu@gmail.com>
2014-01-09 23:19:31 -0700

    Added validations for presence of username and email on user and tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  #relationships
   belongs_to :base
+
+  #validations
+  validates :username, presence: true
+  validates :email, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "it is created with valid attributes" do 
+    assert users(:one).valid?
+  end
+
+  test "it is invalid without a username" do 
+    users(:one).username = nil
+    refute users(:one).valid?
+  end
+
+  test "it is invalid without an email" do 
+    users(:one).email = nil
+    refute users(:one).valid?
+  end
 end
