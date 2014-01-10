12ae35958630dc49ae05c2832996048eae885141
Billy <navyosu@gmail.com>
2014-01-09 22:45:04 -0700

    Created users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110052024) do
+ActiveRecord::Schema.define(version: 20140110053434) do
 
   create_table "bases", force: true do |t|
     t.string   "name"
@@ -77,6 +77,16 @@ ActiveRecord::Schema.define(version: 20140110052024) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.integer  "base_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["base_id"], name: "index_users_on_base_id"
 
   create_table "websites", force: true do |t|
     t.string   "url"
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,8 +2,9 @@ require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
 
-  test "it must belong to a user" do 
-    skip
+  test "it must validate the presence of a user id" do 
+    reviews(:one).user_id = nil
+    refute reviews(:one).valid?
   end
 
 end
