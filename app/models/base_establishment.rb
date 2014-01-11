d471729c5da55ae38df9291548067eaca66da7e9
Billy <navyosu@gmail.com>
2014-01-10 20:26:34 -0700

    Added validations and relationships for base establishments

--- app/models/base_establishment.rb
+++ app/models/base_establishment.rb
@@ -1,2 +1,11 @@
 class BaseEstablishment < ActiveRecord::Base
+  #relationships
+  belongs_to :base 
+  belongs_to :establishment
+
+  #validations
+  validates :base_id, presence: true
+  validates :establishment_id, presence: true
+  validates_numericality_of :base_id
+  validates_numericality_of :establishment_id
 end
--- test/models/base_establishment_test.rb
+++ test/models/base_establishment_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class BaseEstablishmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  should belong_to(:base)
+  should belong_to(:establishment)
+    
+  should validate_presence_of(:base_id)
+  should validate_presence_of(:establishment_id)
+  should validate_numericality_of(:base_id)
+  should validate_numericality_of(:establishment_id)
 end
