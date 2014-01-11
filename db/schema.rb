a54788d8a39c7ef5c07a472d9710468bdeb1664a
Billy <navyosu@gmail.com>
2014-01-11 00:57:15 -0700

    Created category table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111031708) do
+ActiveRecord::Schema.define(version: 20140111075558) do
 
   create_table "base_establishments", force: true do |t|
     t.integer  "base_id"
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(version: 20140111031708) do
 
   create_table "branches", force: true do |t|
     t.string "name"
+  end
+
+  create_table "categories", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "email_addresses", force: true do |t|
