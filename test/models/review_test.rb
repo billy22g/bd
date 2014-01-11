fd1641757415db5f3bffa404604d4bb8ab114aa4
Billy <navyosu@gmail.com>
2014-01-10 18:04:57 -0700

    Added shoulda gem to make shoulda-matchers work in tests, also tested establishment belongs_to relationships

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
 
-  # should belong_to(:user)
-  # should belong_to(:establishment)
+  should belong_to(:user)
+  should belong_to(:establishment)
 
   test "it must validate the presence of a user id" do 
     reviews(:one).user_id = nil
