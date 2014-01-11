3361108068b9b8359670d925f4ee7d5d8d98aa7f
Billy <navyosu@gmail.com>
2014-01-11 01:25:04 -0700

    Added pseudocode for todos for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@ class Category < ActiveRecord::Base
   #relationships
   has_many :establishment_categories
   has_many :establishments, through: :establishment_categories
+
+  #TODO 
+  #Set up base_categories relationships
   
   #validations
   validates :name, presence: true
