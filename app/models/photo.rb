fc640507f38acc015d193f8096a86abb768553b7
Billy <navyosu@gmail.com>
2014-01-12 20:33:09 -0700

    Added validations to photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,13 @@
 class Photo < ActiveRecord::Base
   #relationships
   belongs_to :establishment
-  
+
   #validations
+  validates :filename, presence: true
+  validates :content_type, presence: true
+  validates :file_size, presence: true
+  validates :establishment_id, presence: true
+
+  validates_numericality_of(:file_size)
+  validates_numericality_of(:establishment_id)
 end
--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -3,5 +3,23 @@ require "test_helper"
 class PhotoTest < ActiveSupport::TestCase
 
   should belong_to(:establishment)
-  
+
+  should validate_presence_of(:filename)
+  should validate_presence_of(:content_type)
+  should validate_presence_of(:file_size)
+  should validate_presence_of(:establishment_id)
+  should validate_numericality_of(:file_size)
+  should validate_numericality_of(:establishment_id)
+
+  test "it ensures filename is formatted properly" do 
+    skip
+  end
+
+  test "it ensures content type is formatted properly" do 
+    skip
+  end
+
+  test "it ensures file size is formatted properly and within certain parameters" do 
+    skip
+  end
 end
